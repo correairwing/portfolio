1fb1c5feab70937724c862f846dfba462b040311
Irwing <irwingcorrea95@gmail.com>
2023-07-27 12:06:18 -0300

    estilizando sidebar

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -28,6 +28,18 @@ const Projetos = () => (
       <li>
         <Projeto />
       </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
     </Lista>
   </section>
 )
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/correairwing.png" />
-    <Titulo fontSize={20}>Irwing C.</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Irwing C.</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        correairwing
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Front-end Dev
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
